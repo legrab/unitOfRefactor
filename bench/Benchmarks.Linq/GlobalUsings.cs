ed0f5f78e8b3f1b66195495174d90ac2f2fabc63
legrab <b.geller@advanced-engineering.at>
2025-04-17 10:13:35 +0200

    bench: add short benchmark run and run at program

---
+++ bench/Benchmarks.Linq/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// @Copyright (C) https://github.com/legrab, 2025
+// Free to use under GPLv3 - share & improve with credit
+
+global using BenchmarkDotNet.Attributes;
+global using BenchmarkDotNet.Reports;
+global using BenchmarkDotNet.Running;
+global using JetBrains.Annotations;
